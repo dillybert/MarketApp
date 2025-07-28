9b6e5e0af88687f0e13f2fdae510c7350aaecf1f
dillybert <nurkadyr.t@gmail.com>
2025-07-28 21:23:44 +0500

    Test commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "2.5"
+        versionName = "2.5-patch"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
