a8bb9425a39820eab96ba3b655e0fa2432d0343f
dillybert <nurkadyr.t@gmail.com>
2025-07-28 17:01:23 +0500

    Patch commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "2.3"
+        versionName = "2.3-patch"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
